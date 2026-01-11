144afc1ade84639ad4b3a9e5e8d60225644626a4
ckgauro <ckgauro@gmail.com>
2026-01-10 20:04:26 -0500

    MicroServices pattern

---
+++ Saga Pattern Choregraphy/common-bom/src/test/java/com/gauro/commonbom/CommonBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauro.commonbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Saga Pattern Choregraphy/gateway/src/test/java/com/gauro/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauro.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
